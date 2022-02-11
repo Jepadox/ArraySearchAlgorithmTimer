0367cca95bcbe661fd8175d18f207ed813221a11
jepadox <jlaca020@fiu.edu>
2022-02-11 10:52:29 -0500

    Add the sequentialSearch method

--- src/main/java/SearchAlgorithms.java
+++ src/main/java/SearchAlgorithms.java
@@ -1,3 +1,17 @@
 public class SearchAlgorithms {
 
+    //sequential search returning true if found
+    public static boolean sequentialSearch(int[] arr, int x){
+        boolean found = false;
+
+        for(int num : arr){
+            //if x is found in arr, end search
+            if(num == x){
+                found = true;
+                break;
+            }
+        }
+
+        return found;
+    }
 }
