ab37ed1245b2b70809f47c3aa6b667690596149c
jepadox <jlaca020@fiu.edu>
2022-02-11 10:34:12 -0500

    Add the fillArray method

--- src/main/java/ArrayUtilities.java
+++ src/main/java/ArrayUtilities.java
@@ -1,4 +1,12 @@
+import java.util.Random;
+
 public class ArrayUtilities {
+    //fill array with random numbers
+    public static void fillArray(int[] arr){
+        Random r = new Random();
 
-
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = r.nextInt();
+        }
+    }
 }
